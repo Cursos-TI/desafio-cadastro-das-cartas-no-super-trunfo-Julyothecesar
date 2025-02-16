0376e40686ffaaa8445556b6b12bae31689def60
Julyo <julyorocesar@gmail.com>
2025-02-16 14:35:35 -0300

    desenvolmimentos julyo

---
+++ .vscode/program_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main () {
+
+
+    int idade = 25;
+float altura = 1.85;
+char opcao = 'j';
+char nome[20] = "Julyo";
+
+printf ("Nome:%s\n", nome);
+printf ("Altura:%f\n", altura);
+printf ("Opção:%c\n", opcao);
+printf ("Idade:%d\n", idade);
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ olamundo/Somas.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, soma;
+
+    // Solicita ao usuário para inserir dois números inteiros
+    printf("Digite o primeiro número: ");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo número: ");
+    scanf("%d", &num2);
+
+    // Calcula a soma dos dois números
+    soma = num1 + num2;
+
+    // Exibe o resultado
+    printf("A soma de %d e %d é: %d\n", num1, num2, soma);
+
+    return 0;
+}
