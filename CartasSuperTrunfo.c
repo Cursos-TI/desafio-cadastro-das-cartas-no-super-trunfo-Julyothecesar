d5d5084483687b21bd4922368c56aa83a679f53b
Julyo <julyorocesar@gmail.com>
2025-02-17 00:07:59 -0300

    finalizei o codigo.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,50 +1,49 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
-    int code, pturistc;
-    char name;
+
+    // Declarei as variaveis*
+
+    int pturistc;
+    char code, name[40];
     int population;
     float area;
     double pib;
 
+    // Inseri as entradas com seus respequitivos especificadores (%d, %s, %f, %lf) na saida:)
+
     printf ("Digite o codigo da cidade:\n");
-    scanf ("%s", &code);
+    scanf ("%d", &code);
 
     printf ("Nome: \n");
-    scanf ("%s", &name);
+    scanf ( "%s", name);
 
     printf ("Qual a população: \n");
-    scanf ("%c", &population);
+    scanf ("%d", &population);
 
-    printf ("Isira a extenção do territorio: \n");
-    scanf ("%s", &area);
+    printf ("Isira a extensão do territorio: \n");
+    scanf ("%f", &area);
 
     printf ("Qual o PIB: \n");
-    scanf ("%c", &pib);
+    scanf ("%lf", &pib);
     
     printf ("Numero de Pontos Turisticos: \n");
-    scanf ("%s", &pturistc);
+    scanf ("%d", &pturistc);
 
-    printf ("Codigo da cidade: %s- Nome do pais: %s", code, name);
+    // Dei saida as informações obitidas nas linas acima:)
+
+    printf ("Codigo da cidade: %d - Nome do pais: %s \n", code, name);
+    printf ("População: %d - Area: %f \n", population, area);
+    printf ("Pid: %lf - Pontos turisticos: %d", pib, pturistc);
     
 
 
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-    // Começarei apartira da 19:00.
+    // Por fim, o codigo funciona, mas temos o probleno com o buffer do enter, assim não
+    // conseguimos inserir palavras com espaço como "São Paulo" o problema pode ser resolvido 
+    // com uma insersão com a fução "void" ou "getchar":)
+   
+   
     return 0;
+
 }
