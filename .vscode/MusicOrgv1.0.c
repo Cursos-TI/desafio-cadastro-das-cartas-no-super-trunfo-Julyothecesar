d8d53d369b4c95384daa7b99dc28b0ee88959e4e
Julyo <julyorocesar@gmail.com>
2025-02-18 20:49:23 -0300

    Comecei um novo programa...

---
+++ .vscode/MusicOrgv1.0.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main () {
+ 
+
+    int code, paste; 
+    char musicname[40];
+
+
+    printf ("Digite o nome da Musica: \n");
+    scanf ("%s", musicname);
+
+    printf ("Digite o codigo da musica: \n");
+    scanf ("%d", code);
+
+    printf ("Digite o codigo da pasta: \n");
+    scanf ("%d", paste);
+
+
+    printf ("Nome: %s \n", musicname);
+    printf ("Codigo da paste e da musica: %d, %d \n",code, paste);
+
+    return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
